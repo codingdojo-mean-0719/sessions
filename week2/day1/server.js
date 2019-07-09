5f4bd8ad3b1b54423972045c2d55f9ab75d19172
dojo-json <jfranz@codingdojo.com>
2019-07-09 10:18:09 -0700

    middleware

--- week2/day1/server.js
+++ week2/day1/server.js
@@ -5,12 +5,35 @@ const path = require('path');
 const port = process.env.PORT || 8000;
 const app = express();
 
+const logger = require('./server/middleware/logger');
+
+
+console.log(logger);
+
 const names = ['Sally', 'Jason', "George"];
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(function (request, response, next) {
+  console.log(request.body);
+
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger);
+
+
+function isLoggedIn(request, response, next) {
+  const loggedIn = true;
+
+  if (!loggedIn) {
+    return next(new Error(`You must be logged in`));
+  }
+
+  next();
+}
 
 app.get('/', function (request, response) {
   // console.log(request);
@@ -18,7 +41,7 @@ app.get('/', function (request, response) {
   response.render('index');
 });
 
-app.post('/names', function (request, response) {
+app.post('/names', [isLoggedIn], function (request, response) {
   
   const firstName = request.body.firstName;
   
@@ -26,14 +49,21 @@ app.post('/names', function (request, response) {
 
   names.push(firstName);
 
-  // response.render('names', { name: firstName, names });
+  response.render('names', { name: firstName, names });
 
-  response.redirect('/');
+  // response.redirect('/');
 });
 
 app.get('/names/:name_id', function (request, response) {
   console.log(request.params.name_id);
   response.send(names[request.params.name_id]);
-})
+});
+
+
+app.use(function (error, request, response, next) {
+  console.log('this is aan error', error.message);
+
+  response.redirect('/');
+});
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
